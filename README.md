a882fccd8169530e779c98a78064abafa14dc6f5
vanstee <patrick@vanstee.me>
2013-05-20 18:47:23 -0400

    Show timestamps as you receive them

---
+++ README.md
@@ -0,0 +1,10 @@
+Clockerl
+--------
+
+JSON the client expects.
+
+```json
+{
+  "type": "clock", "data": "136989867.811151"
+}
+```
--- docroot/clockerl.js
+++ docroot/clockerl.js
@@ -1,3 +1,8 @@
 $(function() {
   var socket = new WebSocket('ws://localhost:8080/websockets')
+
+  socket.onmessage = function(event) {
+    var message = JSON.parse(event.data)
+    $('ul.clock').append($('li').text(message['data'])
+  };
 })
